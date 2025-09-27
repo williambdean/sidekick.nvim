5335ad94baca9463dc02a6c767bda057dcf99992
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-09-27 15:02:07 +0200

    feat(util): split_words / split_chars

--- lua/sidekick/util.lua
+++ lua/sidekick/util.lua
@@ -34,4 +34,53 @@ function M.debounce(fn, ms)
   end
 end
 
+--- UTF-8 aware word splitting. See |keyword|
+---@param str string
+function M.split_words(str)
+  if str == "" then
+    return {}
+  end
+
+  local ret = {} ---@type string[]
+  local word = {} ---@type string[]
+  local starts = vim.str_utf_pos(str)
+
+  local function flush()
+    if #word > 0 then
+      ret[#ret + 1] = table.concat(word)
+      word = {}
+    end
+  end
+
+  for idx, start in ipairs(starts) do
+    local stop = (starts[idx + 1] or (#str + 1)) - 1
+    local ch = str:sub(start, stop)
+    if vim.fn.charclass(ch) == 2 then -- iskeyword
+      word[#word + 1] = ch
+    else
+      flush()
+      ret[#ret + 1] = ch
+    end
+  end
+
+  flush()
+  return ret
+end
+
+--- UTF-8 aware character splitting
+---@param str string
+function M.split_chars(str)
+  if str == "" then
+    return {}
+  end
+
+  local ret = {} ---@type string[]
+  local starts = vim.str_utf_pos(str)
+  for i = 1, #starts - 1 do
+    table.insert(ret, str:sub(starts[i], starts[i + 1] - 1))
+  end
+  table.insert(ret, str:sub(starts[#starts], #str))
+  return ret
+end
+
 return M
--- tests/util_spec.lua
+++ tests/util_spec.lua
@@ -1,8 +1,8 @@
 ---@module 'luassert'
 
-local tokenize = require("sidekick.nes.diff").tokenize
+local Util = require("sidekick.util")
 
-describe("tokenize", function()
+describe("split_words", function()
   -- should split a string in alpha / non-alpha parts
   local cases = {
     { "abcd", { "abcd" } },
@@ -23,6 +23,10 @@ describe("tokenize", function()
     { "abc*def", { "abc", "*", "def" } },
     { "abc/def", { "abc", "/", "def" } },
     { "abc=def", { "abc", "=", "def" } },
+    { "café", { "café" } },
+    { "😀", { "😀" } },
+    { "foo😀bar", { "foo", "😀", "bar" } },
+    { "ありがとう", { "あ", "り", "が", "と", "う" } },
     {
       'local diff = require("sidekick.nes.diff").diff(edit)',
       {
@@ -53,7 +57,30 @@ describe("tokenize", function()
 
   for _, case in ipairs(cases) do
     it(case[1] .. " => " .. vim.inspect(case[2]), function()
-      assert.are.same(case[2], tokenize(case[1]))
+      -- vim.o.iskeyword = "@,48-57,_,192-255"
+      assert.are.same(case[2], Util.split_words(case[1]))
+    end)
+  end
+end)
+
+describe("split_chars", function()
+  -- ensure split_chars breaks strings into individual characters
+  local cases = {
+    { "abc", { "a", "b", "c" } },
+    { "abc def", { "a", "b", "c", " ", "d", "e", "f" } },
+    { "abc\tdef", { "a", "b", "c", "\t", "d", "e", "f" } },
+    { "abc\ndef", { "a", "b", "c", "\n", "d", "e", "f" } },
+    { "0.1", { "0", ".", "1" } },
+    { "😀", { "😀" } },
+    { "😀😃", { "😀", "😃" } },
+    { "👍🏼", { "👍", "🏼" } },
+    { "café", { "c", "a", "f", "é" } },
+    { "ありがとう", { "あ", "り", "が", "と", "う" } },
+  }
+
+  for _, case in ipairs(cases) do
+    it(case[1] .. " => " .. vim.inspect(case[2]), function()
+      assert.are.same(case[2], Util.split_chars(case[1]))
     end)
   end
 end)
