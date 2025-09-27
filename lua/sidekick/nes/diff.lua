ef2884e513f45cca69e99fb3d39cca54e4bc8cae
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-09-27 15:31:03 +0200

    feat(diff): diff refactoring

--- lua/sidekick/nes/diff.lua
+++ lua/sidekick/nes/diff.lua
@@ -1,7 +1,31 @@
 local Config = require("sidekick.config")
 local TS = require("sidekick.treesitter")
+local Util = require("sidekick.util")
 
 local M = {}
+
+local INLINE_MAX_LINES = 3
+local INLINE_MAX_INSERT_RATIO = 0.4
+
+---@type vim.text.diff.Opts
+local DIFF_OPTS = {
+  algorithm = "patience",
+  result_type = "indices",
+  indent_heuristic = true,
+  linematch = 10,
+  ctxlen = 0,
+  interhunkctxlen = 0,
+}
+
+---@type vim.text.diff.Opts
+local DIFF_INLINE_OPTS = {
+  algorithm = "minimal",
+  result_type = "indices",
+  linematch = 0,
+  ctxlen = 0,
+  interhunkctxlen = 4,
+  indent_heuristic = false,
+}
 
 ---@alias sidekick.DiffText {text: string, lines: string[], virt_lines: sidekick.TSVirtualLines}
 
@@ -31,14 +55,11 @@ end
 
 ---@param a string[]
 ---@param b string[]
-function M._diff(a, b)
+---@param opts vim.text.diff.Opts
+function M._diff(a, b, opts)
   local txt_a = table.concat(a, "\n")
   local txt_b = table.concat(b, "\n")
-
-  local diff_opts = vim.deepcopy(Config.nes.diff)
-  diff_opts.inline = nil
-  diff_opts.result_type = "indices"
-  return vim.text.diff(txt_a, txt_b, diff_opts) --[[@as (integer[][])]]
+  return vim.text.diff(txt_a, txt_b, opts) --[[@as (integer[][])]]
 end
 
 ---@param edit sidekick.NesEdit
@@ -81,13 +102,24 @@ end
 
 ---@param diff sidekick.Diff
 function M.diff_lines(diff)
-  local hunks = M._diff(diff.from.lines, diff.to.lines)
+  local hunks = M._diff(diff.from.lines, diff.to.lines, DIFF_OPTS)
   for _, hunk in ipairs(hunks) do
     local ai, ac, bi, bc = unpack(hunk)
 
-    local token_hunks = ac >= 1 and bc >= 1 and M.diff_tokens(diff, hunk)
-    if token_hunks then
-      vim.list_extend(diff.hunks, token_hunks)
+    local inline_hunks = {} ---@type sidekick.diff.Hunk[]
+    if Config.nes.diff.inline and ac == bc and ac >= 1 and ac <= INLINE_MAX_LINES then
+      for i = 0, ac - 1 do
+        local line_hunks = M.diff_inline(diff, ai + i, bi + i)
+        if not line_hunks then
+          inline_hunks = {}
+          break
+        end
+        vim.list_extend(inline_hunks, line_hunks)
+      end
+    end
+
+    if #inline_hunks > 0 then
+      vim.list_extend(diff.hunks, inline_hunks)
     else
       local row = diff.range.from[1] + ai - 1
       ---@type sidekick.diff.Hunk
@@ -111,103 +143,86 @@ function M.diff_lines(diff)
           row = row + (ac > 0 and ac - 1 or 0),
           col = 0,
           hl_eol = true,
-          virt_lines = vim.list_slice(diff.to.virt_lines, bi, bi + bc - 1),
+          virt_lines = TS.highlight_ws(vim.list_slice(diff.to.virt_lines, bi, bi + bc - 1), {
+            leading = "SidekickDiffContext",
+            trailing = "SidekickDiffContext",
+          }),
         })
       end
     end
   end
 end
 
----@param str string
----@return string[]
-function M.tokenize(str)
-  local parts, i, len = {}, 1, #str
-  while i <= len do
-    local from, to = str:find("^[%w_]+", i)
-    if from then
-      table.insert(parts, str:sub(i, to))
-      i = to + 1
-    else
-      table.insert(parts, str:sub(i, i))
-      i = i + 1
-    end
-  end
-  assert(str == table.concat(parts, ""), "tokenize mismatch")
-  return parts
-end
-
 ---@param diff sidekick.Diff
 ---@param line_idx integer
----@param line_count integer
 ---@param vl_all sidekick.TSVirtualLines
-function M._index(diff, line_idx, line_count, vl_all)
+function M._index(diff, line_idx, vl_all)
   local toks = {} ---@type string[]
   local vl = {} ---@type sidekick.TSVirtualText
-  local index = {} ---@type table<integer, {row: integer, col: integer, end_col: integer}>
-  index[0] = { row = diff.range.from[1] + line_idx - 1, col = 0, end_col = 0 }
-  for l = line_idx, line_idx + line_count - 1 do
-    local col = 0
-    for _, t in ipairs(vl_all[l] or {}) do
-      local parts = M.tokenize(t[1])
-      -- parts = { t[1] } -- FIXME: disable tokenization for now
-      -- parts = vim.split(t[1], "", { plain = true }) -- FIXME: char by char for now
-      for _, p in ipairs(parts) do
-        local idx = #toks + 1
-        toks[idx] = p
-        vl[idx] = { p, t[2] }
-        index[idx] = {
-          row = diff.range.from[1] + l - 1,
-          col = col,
-          end_col = col + #p,
-        }
-        col = col + #p
-      end
+  local index = {} ---@type table<integer, {col: integer, end_col: integer}>
+  index[0] = { col = 0, end_col = 0 } -- needed for insertions before the first token
+  for _, t in ipairs(vl_all[line_idx] or {}) do
+    local parts = Util.split_words(t[1])
+    parts = Util.split_chars(t[1])
+    for _, p in ipairs(parts) do
+      local idx = #toks + 1
+      toks[idx] = p
+      vl[idx] = { p, t[2] }
+      index[idx] = { col = index[idx - 1].end_col, end_col = index[idx - 1].end_col + #p }
     end
   end
+  -- needed for insertions after the last token
+  index[#toks + 1] = { col = index[#index].end_col, end_col = index[#index].end_col }
+  setmetatable(index, {
+    __index = function()
+      return index[#index]
+    end,
+  })
   return toks, index, vl
 end
 
 ---@param diff sidekick.Diff
----@param offset integer[]
-function M.diff_tokens(diff, offset)
-  local a_toks, a_index = M._index(diff, offset[1], offset[2], diff.from.virt_lines)
-  local b_toks, b_index, b_vl = M._index(diff, offset[3], offset[4], diff.to.virt_lines)
+---@param from_idx integer
+---@param to_idx integer
+---@return sidekick.diff.Hunk[]?
+function M.diff_inline(diff, from_idx, to_idx)
+  local a_toks, a_index = M._index(diff, from_idx, diff.from.virt_lines)
+  local b_toks, b_index, b_vl = M._index(diff, to_idx, diff.to.virt_lines)
 
-  local hunks = M._diff(a_toks, b_toks)
+  local hunks = M._diff(a_toks, b_toks, DIFF_INLINE_OPTS)
+  if #hunks == 0 then
+    return {}
+  end
+
   local ret = {} ---@type sidekick.diff.Hunk[]
+  local row = diff.range.from[1] + from_idx - 1
+  local delete_len, insert_len = 0, 0
 
-  for _, hunk in ipairs(hunks) do
-    local ai, ac, bi, bc = unpack(hunk)
-    local row = a_index[ai].row
-    ---@type sidekick.diff.Hunk
+  for _, token_hunk in ipairs(hunks) do
+    local ai, ac, bi, bc = unpack(token_hunk)
+    local a_from = a_index[ai]
     local h = {
       kind = ac > 0 and bc > 0 and "change" or ac > 0 and "delete" or "add",
-      pos = { row, a_index[ai].col },
+      pos = { row, a_from.col },
       inline = true,
       extmarks = {},
     }
+
     if ac > 0 then
-      if a_index[ai + ac - 1].row > a_index[ai].row then
-        return -- multiline hunk
-      end
+      local a_to = a_index[ai + ac - 1]
+      delete_len = delete_len + a_to.end_col - a_from.col
       table.insert(h.extmarks, {
         row = row,
-        col = a_index[ai].col,
-        end_col = a_index[ai + ac - 1].end_col,
+        col = a_from.col,
+        end_col = a_to.end_col,
         hl_group = "SidekickDiffDelete",
       })
     end
+
     if bc > 0 then
-      if b_index[bi + bc - 1].row > b_index[bi].row then
-        return -- multiline hunk
-      end
-      local col = a_index[ai].col
-      if ac == 0 then
-        col = a_index[ai].end_col
-        h.pos[2] = math.max(col - 1, 0)
-      else
-        col = a_index[ai + ac - 1].end_col
-      end
+      insert_len = insert_len + b_index[bi + bc - 1].end_col - b_index[bi].col
+      local col = ac > 0 and a_index[ai + ac - 1].end_col or a_from.end_col
+      h.pos[2] = ac > 0 and col or math.max(col - 1, 0)
       table.insert(h.extmarks, {
         row = row,
         col = col,
@@ -218,7 +233,10 @@ function M.diff_tokens(diff, offset)
     end
     table.insert(ret, h)
   end
-  return ret
+
+  local new_len = #(diff.to.lines[to_idx] or "")
+  local insert_ratio = insert_len / new_len
+  return insert_ratio < INLINE_MAX_INSERT_RATIO and ret or nil
 end
 
 return M
