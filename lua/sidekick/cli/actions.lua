8ebbd7578bcdd345b81ab0d3e6776133d6b0d140
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-03 10:11:16 +0200

    fix(cli): fix insert mode for prompt action. Closes #50

--- lua/sidekick/cli/actions.lua
+++ lua/sidekick/cli/actions.lua
@@ -3,14 +3,18 @@
 local M = {}
 
 function M.prompt(t)
-  local Cli = require("sidekick.cli")
-  Cli.prompt(function(prompt)
-    vim.schedule(function()
-      vim.cmd.startinsert()
+  vim.cmd.stopinsert() -- needed, since otherwise Neovim will do this
+
+  vim.schedule(function()
+    local Cli = require("sidekick.cli")
+    Cli.prompt(function(prompt)
+      vim.schedule(function()
+        vim.cmd.startinsert()
+      end)
+      if prompt then
+        t:send(prompt .. "\n")
+      end
     end)
-    if prompt then
-      t:send(prompt .. "\n")
-    end
   end)
 end
 
